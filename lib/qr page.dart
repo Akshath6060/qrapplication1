bfcf1ace9e6f2c5073e5e0ab4585a6c9d21a3804
akshath <okakshath123@gmail.com>
2024-03-13 16:19:56 +0530

    Initial commit

---
+++ lib/qr page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+class Qr extends StatefulWidget {
+  const Qr({super.key});
+
+  @override
+  State<Qr> createState() => _QrState();
+}
+
+class _QrState extends State<Qr> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.teal,
+      body:Center(
+        child: Column(
+          children: [
+            Icon(Icons.account_circle_rounded,size:200,),
+            SizedBox(height: 100,),
+            QrImageView(
+              data: '1234567890',
+              version: QrVersions.auto,
+              size: 200.0,
+            ),
+            TextButton(onPressed: null, child: Text('scan'))
+
+          ],
+        ),
+      ) ,
+    );
+  }
+}
